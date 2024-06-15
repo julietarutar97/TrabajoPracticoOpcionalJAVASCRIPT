469811fc47f6c593844fbc228d504c90e906cfbc
julietarutar97 <julietarutar97@gmail.com>
2024-06-15 18:29:08 -0300

    Corrección de la función cuadrado del ejercicio 1

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -3,10 +3,12 @@
 let numeros = [0,1,2,3,4,5,6,7,8,9] ;
 
 function cuadrado(arrayDeNumeros){
-    for(nuero of numeros){
-        console.log(Math.pow(nuero,2))
+ let numerosAlCuadrado=[];
+    for(numero of numeros){
+       let resultado = Math.pow(numero,2);
+        numerosAlCuadrado.push(resultado);
     }
+    return numerosAlCuadrado;
 }
 
-
-cuadrado(numeros);
+console.log(cuadrado(numeros));
---
+++ js/ejercicio2.js
@@ -0,0 +1,7 @@
+//Uso de for y Cadenas: Crea una función que reciba una cadena y devuelva una nueva cadena con las vocales en mayúsculas. Utiliza un bucle for para iterar sobre los caracteres de la cadena.
+
+let letras = ["Julieta", "Rutar"]
+
+function convertirVocalesMayusculas(listaDeCadenas){
+    
+}
