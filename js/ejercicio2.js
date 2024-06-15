9ce3d6fcdf376b5905f107349a860a76d7e5d60e
julietarutar97 <julietarutar97@gmail.com>
2024-06-15 18:49:55 -0300

    Ejercicio 2 terminado

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,7 +1,26 @@
-//Uso de for y Cadenas: Crea una función que reciba una cadena y devuelva una nueva cadena con las vocales en mayúsculas. Utiliza un bucle for para iterar sobre los caracteres de la cadena.
+let letras = ["Julieta", "Rutar"];
 
-let letras = ["Julieta", "Rutar"]
 
-function convertirVocalesMayusculas(listaDeCadenas){
-    
+function convertirVocalesMayusculas(cadena) {
+    let nuevaCadena = '';
+    let vocales = ['a', 'e', 'i', 'o', 'u'];
+    let tamanioLetras = cadena.length;
+
+    for (let i = 0; i < tamanioLetras; i++) {
+        let palabra = cadena[i];
+        let tamanioPalabra = palabra.length;
+        for (let j = 0; j < tamanioPalabra; j++) {
+            let char = palabra[j];
+            
+            if (vocales.includes(char)) {
+                nuevaCadena += char.toUpperCase(); 
+            } 
+        }
+    }
+
+    return nuevaCadena;
 }
+
+let resultado = letras.map(convertirVocalesMayusculas);
+
+console.log(resultado);
